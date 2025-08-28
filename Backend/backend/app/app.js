6a33bf47b6d8b4289a16402ccab3fc2a6e33657d
aninakwadesmond <aninakwadesmond3@gmail.com>
2025-08-28 22:47:32 +0000

    change commit

--- Backend/backend/app/app.js
+++ Backend/backend/app/app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 //fetching data
 app.get('/login', (req, res) => {
   res.json(Account);
