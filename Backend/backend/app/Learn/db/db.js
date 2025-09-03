b5f379475976e28af263cfe54eb6dc5294b6f00c
aninakwadesmond <aninakwadesmond3@gmail.com>
2025-09-03 14:14:09 +0000

    host database

--- Backend/backend/app/Learn/db/db.js
+++ Backend/backend/app/Learn/db/db.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv').config();
 // console.log(process.env.name, 'hello232220');
 const mysql = require('mysql2');
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.DATABASE}`;
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
 
 const db = mysql.createConnection(urlDB);
 // This code reads the connection details from environment variables
